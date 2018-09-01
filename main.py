74e8eff1702de439fba0e92c06c9294f1d303e1f
Jianxing <jecing@qq.com>
2018-09-02 00:30:04 +0800

    bug fix

--- main.py
+++ main.py
@@ -48,5 +48,7 @@ for i in range(data.get_num_lines(path.join(path.dirname(__file__),
     p.apply_async(data.sng_from_line_2_queue,
                   (i, q,)
                   )
+p.close()
+p.join()
 
 scaffold_dict, dataset = data.protobuf_from_queue(q)

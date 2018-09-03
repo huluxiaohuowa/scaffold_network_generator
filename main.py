c1a3b54d2940d769313a4af740a42f108c63bae1
Jianxing <jecing@qq.com>
2018-09-03 17:40:46 +0800

    好烦啊，怎么弄

--- main.py
+++ main.py
@@ -1,37 +1,4 @@
-# from multiprocessing import Pool,Manager
-# from os import path
-# import data
-# import linecache
 
-# num_process = 30
-# input_file = 'input.txt'
-# output_file = 'output.txt'
-# input_dir = path.join(path.dirname(__file__),
-#                       'data',
-#                       'datasets',
-#                       input_file
-#                       )
-#
-# num_lines = -1
-# for num_lines, line in enumerate(open(input_dir, 'r')):
-#     pass
-# num_lines += 1
-#
-#
-# def get_smiles(idx_line):
-#     return linecache.getline(input_dir, idx_line).strip()
-#
-#
-# if __name__ == '__main__':
-#     print(num_lines)
-#     for i in range(num_lines):
-#         try:
-#             data.output_to_file(get_smiles(i+1))
-#         except:
-#             print(get_smiles(i+1))
-# from data import *
-#
-# dic_scaffold, pro_scaffold = get_sng_protobuf()
 import data
 from multiprocessing import Manager
 # from os import path

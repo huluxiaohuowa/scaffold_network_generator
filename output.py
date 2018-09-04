37fe5a387bb429032512eb4b3674d08e1ee14878
Jianxing <jecing@qq.com>
2018-09-04 11:23:23 +0800

    arg parse

---
+++ output.py
@@ -0,0 +1,38 @@
+
+import data
+from multiprocessing import Manager
+from data import *
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--file_input", help="The location of input file", type=str)
+parser.add_argument("--file_output", help="The location of output file", type=str)
+args = parser.parse_args()
+# from os import path
+
+if not args.file_input:
+    file_input = path.join(path.dirname(__file__),
+                           'data',
+                           'datasets',
+                           'input.txt')
+else:
+    file_input = args.file_input
+
+if not args.file_output:
+    file_output = path.join(path.dirname(__file__),
+                           'data',
+                           'datasets',
+                           'scaffolds.bin')
+else:
+    file_output = args.file_output
+
+q = Manager().Queue()
+
+data.sng_to_queue(q, processes=30, file=file_input)
+
+dic = data.data_from_queue(q)
+
+print(dic.smiles_scaffold[scaffold_mol_idx(2)])
+
+with open(file_output, 'wb') as f:
+    f.write(dic.SerializeToString())
+
